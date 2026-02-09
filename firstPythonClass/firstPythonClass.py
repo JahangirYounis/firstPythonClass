eaebf571208bb6f91f24d3ee6fd56922326eb5aa
JahangirYounis <khaskheliladla@gmail.com>
2026-02-09 13:15:58 +0500

    Add project files.

---
+++ firstPythonClass/firstPythonClass.py
@@ -0,0 +1,197 @@
+res = 1;
+
+for x in range(5, 1, -1):
+
+    res *= x;
+    print(res," * ",x, " = " ,res)
+    
+    
+print(res)
+# import math 
+
+# num = int(input("Enter number: "))
+
+# if num <= 1:
+#     print(num, "is not a prime number")
+# else:
+#     for z in range(2, num):
+         
+#         if num % z == 0:
+#             print("its not a prime number")
+#             break
+#     else:
+#          print(num, "is a prime number")
+
+        
+# n = int(input("Enter Number:"))
+# for x in range(n,0, -1):  
+#     print(x);
+
+# def show():   
+#     print("hello")
+
+# show()
+
+
+# lisht = [23,45,67,26,67,87, 70, 93,38]
+# print(lisht.append(4))
+# print(lisht.remove(4))
+# print(lisht.pop(2))
+# print(lisht)
+# lisht.append(2)
+# lisht.sort()
+# lisht = ["Jahangir","Ali","Khaskheli","muhammad","Younis"]
+# print(lisht[3])
+# myList = [10,20,30,40,50]
+# for x in myList :
+#     print(x)
+# myData = [ "Jahangir", 12, 16,"th "]
+# print(myData)
+# a = 0;
+# b = 1;
+# num = int(input("How many times you want to print: "))
+# while i <= num:
+#     print(a, end="  ");
+#     res = a + b
+#     a = b
+#     b = res 
+    
+
+# # n = int(input("How many terms? "))
+
+# # a = 0
+# # b = 1
+# # i = 1
+
+# # while i <= n:
+# #     print(a, end=" ")
+# #     c = a + b
+# #     a = b
+# #     b = c
+# #     i += 1
+
+# # dic = {
+# #     'a': 'hello',
+# #     'b': 'world',
+# #     }
+# # for key, value in dic.items():
+# #     if key == 'a':
+# #         print(value)
+# # i = 6
+# # while i > 1:
+# #     print(i)
+# #     i -=1
+
+# # from operator import le
+
+
+# # i = 1
+# # j = i+5
+# # while i<6:
+# #      i+=1
+# #      print(i*j)
+
+
+
+
+
+
+
+
+
+# # print("List of Odd numbers: ")
+# # for i in range(50):
+# #     if (i%2 != 0):
+# #         print(i)
+     
+# # a = input("Enter First Number: ")
+# # b = input("Enter 2nd Number: ")
+# # print("Average of both is: ",((a+b)/2));
+
+# # light = input("light colour is: ")
+# # if(light == "green"):
+# #     print("You can go!")
+# # elif(light == "red"):
+# #     print("light is red! stop here!")
+# # elif(light == "yellow"):
+# #     print("light is yellow!")
+# # marks = input("enter your marks: ");
+# # # '>=' not supported between instances of 'str' and 'int'
+# # if (marks >= 101 or marks <= 0 ):
+# #     print("Please Enter marks bitween the range 100 and 1")
+# # if (marks >= 90):
+# #     print("You goted A1 grade!")
+# # elif (marks >= 75):
+# #     print("You goted A grade!")
+# # elif (marks >= 60):
+# #     print("You goted B grade!")
+# # elif (marks >= 50):
+# #     print("You goted C grade!")
+# # elif (marks >= 40):
+# #     print("You goted D grade!")
+# # else:
+# #     print("oh! You are failed!")
+# # Age = input("Enter your age ");
+# # if (Age >= 18):
+# #         print("You can vote!")
+# # else:
+# #         print("You can't vote!")
+
+# # name = input("Name: ")
+# # age =  input("Age: ")
+# # price = input("Price: ")
+
+# # print("My name is: ", name, "My age is: ", age, "price of Laptop is: ",price, "rupees.")
+
+
+# # a,b = 12,5;
+# # c = a//b;
+# # print(c);
+
+# # a,b = -12,5;
+# # c = a//b;
+# # print(c);
+
+# # a,b = 12,-5;
+# # c = a//b;
+# # print(c);
+
+# # a,b = -5,2;
+# # c = a%b;
+# # print(c);
+
+# # a,b = 5,2;
+# # c = a%b;
+# # print(c);
+
+# # a,b = -5,-2;
+# # c = a%b;
+# # print(c);
+
+
+
+# # a,b = 1,2;
+
+# # print("a//b = ",a//b , ": while a/b = ", a/b);
+
+
+# # a,b = 8,4;
+# # c = a//b;
+# # print(c);
+
+# # a,b = 1,8;
+# # c = a/b;
+# # print(c);
+
+# # a,b = 2, 3; 
+# # c = "@";
+# # print(a, "*", c, "*", b ,": ",a*b*c)
+# # print("a = ",a,", b = ",b)
+
+# # print("Hello Python from Visual Studio 2022")
+
+# # eee = "7"
+# # print(2* eee * 8)
+
+# # name = input("What is your name: ")
+# # print("My Name is "name)
